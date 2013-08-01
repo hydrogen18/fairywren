05220ee0017dafe762c3e558cab048302d8b284c
Eric Urban <hydrogen18@gmail.com>
2013-08-01 19:32:36 -0400

    added caching of torrent pages

--- html/js/torrents.js
+++ html/js/torrents.js
@@ -39,98 +39,142 @@ $(document).ready(function(){
 	
 	$("#main").tabs();
 	$("#torrentBrowseBar").hide();
-	Fairywren.showTorrents();
+	Fairywren.loadTorrentsForPage();
 });
 
 Fairywren.torrents = {};
 Fairywren.torrents.pageSize = 20;
 Fairywren.torrents.page = 0;
-
-Fairywren.torrents.maxPage = null;
+Fairywren.torrents.pages = null;
+Fairywren.torrents.numPages = null;
 
 Fairywren.flipPage = function(dist)
 {
 	
+	//Move the page the requested amount
 	Fairywren.torrents.page += dist;
-	if (Fairywren.torrents.page < 0 || Fairywren.torrents.page >= Fairywren.torrents.maxPage)
+	//Check to see if bounds have been exceeded. If so, back out the change
+	//and do nothing
+	if (Fairywren.torrents.page < 0 || Fairywren.torrents.page >= Fairywren.torrents.numPages)
 	{
 		Fairywren.torrents.page -= dist;
 		return;
 	}
 	
-	Fairywren.showTorrents();
+	//Load the torrents for this page
+	Fairywren.loadTorrentsForPage();
 }
+
+Fairywren.loadTorrentsForPage = function(clearCache)
+{
+	//If clearcache is specified and is true,
+	//then remove any chaced pages
+	if(! (clearCache===undefined || clearCache === null) && clearCache)
+	{
+		Fairywren.torrents.pages = null;
+	}
+	
+	var page = Fairywren.torrents.page;
+	//If the page is not loaded in the cache, then request it
+	if (Fairywren.torrents.pages===null || Fairywren.torrents.pages[page] === null)
+	{
+		//Request the torrents list
+		jQuery.get("api/torrents",{'resultSize': Fairywren.torrents.pageSize, 'subset':Fairywren.torrents.page}).
+			done(
+				function(data)
+				{
+					if("error" in data)
+					{
+						$("#message").text(data.error);
+					}
+					else
+					{
+						Fairywren.torrents.numPages = data.numSubsets;
+						
+						//Create the array of torrent pages if needed
+						if (Fairywren.torrents.pages === null || Fairywren.torrents.pages.length != Fairywren.torrents.numPages)
+						{
+							Fairywren.torrents.pages = [];
+							for(var i = 0 ; i < Fairywren.torrents.numPages ; ++i)
+							{
+								//No pages are loaded by default
+								Fairywren.torrents.pages.push(null);
+							}
+						}
+						
+						//Add this page to the list
+						Fairywren.torrents.pages[page] = data.torrents;
+						
+						//Update the DOM
+						Fairywren.showTorrents();
+					}
+				}).
+			fail(
+				function()
+				{
+					$("#message").text("Server error");
+				}
+			);;
+	}
+	else
+	{
+		//Just update the DOM immediately
+		Fairywren.showTorrents();
+	}
+			
+}
+
 Fairywren.showTorrents = function()
 {
-
 	var torrentTable = $("#torrentTable");
 	torrentTable.find('tr:gt(0)').remove();
-	jQuery.get("api/torrents",{'resultSize': Fairywren.torrents.pageSize, 'subset':Fairywren.torrents.page}).
-		done(
-			function(data)
+	var page = Fairywren.torrents.page;
+	$("#pageNumbers").text((page +1 )+ ' / ' + Fairywren.torrents.numPages);
+	
+	var pageset = Fairywren.torrents.pages[page];
+	for(i in pageset)
+	{
+		var title = pageset[i].title;
+		var uploadTime = pageset[i].creationDate.substr(0,19);
+		var uploader = pageset[i].creator.name;
+		var downloadUrl = pageset[i].metainfo.href;
+		var lengthInBytes = pageset[i].lengthInBytes;
+		var seeds = pageset[i].seeds;
+		var leeches = pageset[i].leeches;
+		
+		var adjustedLength = lengthInBytes;
+		var adjustedUnits = 'bytes';
+		
+		var ADJUSTMENTS = ['kilobytes','megabytes','gigabytes'];
+		var SCALE = 1024;
+		for(var i = 0;i < ADJUSTMENTS.length; ++i)
+		{
+			if(SCALE > adjustedLength )
 			{
-				if("error" in data)
-				{
-					$("#message").text(data.error);
-				}
-				else
-				{
-					Fairywren.torrents.maxPage = data.numSubsets;
-					
-					$("#pageNumbers").text((Fairywren.torrents.page +1 )+ ' / ' + Fairywren.torrents.maxPage);
-					
-					for(i in data.torrents)
-					{
-						var title = data.torrents[i].title;
-						var uploadTime = data.torrents[i].creationDate.substr(0,19);
-						var uploader = data.torrents[i].creator.name;
-						var downloadUrl = data.torrents[i].metainfo.href;
-						var lengthInBytes = data.torrents[i].lengthInBytes;
-						var seeds = data.torrents[i].seeds;
-						var leeches = data.torrents[i].leeches;
-						
-						var adjustedLength = lengthInBytes;
-						var adjustedUnits = 'bytes';
-						
-						var ADJUSTMENTS = ['kilobytes','megabytes','gigabytes'];
-						var SCALE = 1024;
-						for(var i = 0;i < ADJUSTMENTS.length; ++i)
-						{
-							if(SCALE > adjustedLength )
-							{
-								break;
-							}
-							var adjustment = Math.pow(SCALE,i+1);
-							adjustedLength = lengthInBytes / adjustment;
-							adjustedUnits = ADJUSTMENTS[i];
-						}
-						
-						
-						var displayLengthFixed = parseInt(adjustedLength) !== adjustedLength;
-						if(displayLengthFixed)
-						{
-							adjustedLength = adjustedLength.toFixed(2);
-						}
-						
-						var row = '<tr><td>' + title + 
-						'&nbsp;<a style="float:right;" class="downloadLink" href="' + downloadUrl + '">Download</a>\
-						<span style="float:right;" >'+ '&uarr;' + seeds  + '&nbsp;&darr;' + leeches + '</span></td>\
-						<td>' + adjustedLength + ' ' + adjustedUnits + '</td>\
-						<td>' + uploadTime + "</td>\
-						<td>" + uploader + "</td></tr>";
-						$("#torrentTable tr:last").after(row);
-					}
-					$("#torrentBrowseBar").show();
-				}
-				
+				break;
 			}
-		).
-		fail(
-			function()
-			{
-				$("#message").text("Server error");
-			}
-		);
+			var adjustment = Math.pow(SCALE,i+1);
+			adjustedLength = lengthInBytes / adjustment;
+			adjustedUnits = ADJUSTMENTS[i];
+		}
+		
+		
+		var displayLengthFixed = parseInt(adjustedLength) !== adjustedLength;
+		if(displayLengthFixed)
+		{
+			adjustedLength = adjustedLength.toFixed(2);
+		}
+		
+		var row = '<tr><td>' + title + 
+		'&nbsp;<a style="float:right;" class="downloadLink" href="' + downloadUrl + '">Download</a>\
+		<span style="float:right;" >'+ '&uarr;' + seeds  + '&nbsp;&darr;' + leeches + '</span></td>\
+		<td>' + adjustedLength + ' ' + adjustedUnits + '</td>\
+		<td>' + uploadTime + "</td>\
+		<td>" + uploader + "</td></tr>";
+		$("#torrentTable tr:last").after(row);
+	}
+	$("#torrentBrowseBar").show();
+
 }
 
 
