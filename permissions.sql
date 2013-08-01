30af06817e1d5201e804a6a35a5ad70095e0444d
Eric Urban <hydrogen18@gmail.com>
2013-07-31 20:57:43 -0400

    added missing permissions needed by webapi

--- permissions.sql
+++ permissions.sql
@@ -2,6 +2,9 @@ GRANT all on torrents to webapi;
 GRANT UPDATE,SELECT on torrents_id_seq to webapi;
 GRANT ALL on users to webapi;
 GRANT UPdATE,SELECT on users_id_seq to webapi;
+GRANT ALL on roles to webapi;
+GRANT UPDATE,SELECT on roles_id_seq to webapi;
+GRANT ALL on rolemember to webapi;
 
 GRANT SELECT on torrents to tracker;
 GRANT SELECT on users to tracker;
