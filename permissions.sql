71c921514cb2d8a3b992822c10b69a027975a18f
ericu <ericu@ubuntu-lts-base.(none)>
2013-07-31 20:13:18 -0400

    fixed typo in permissions file

--- permissions.sql
+++ permissions.sql
@@ -1,7 +1,7 @@
 GRANT all on torrents to webapi;
 GRANT UPDATE,SELECT on torrents_id_seq to webapi;
 GRANT ALL on users to webapi;
-GRANT UPATE,SELECT on users_id_seq to webapi;
+GRANT UPdATE,SELECT on users_id_seq to webapi;
 
 GRANT SELECT on torrents to tracker;
 GRANT SELECT on users to tracker;
