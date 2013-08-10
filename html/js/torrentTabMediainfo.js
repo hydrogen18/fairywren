3ce5b5382a9a1db2bc24d438b950ae0278e8ad9b
Eric Urban <hydrogen18@gmail.com>
2013-08-09 21:51:06 -0400

    came up with a JS pattern to clean up the media info

--- html/js/torrentTabMediainfo.js
+++ html/js/torrentTabMediainfo.js
@@ -5,7 +5,7 @@ Fairywren.torrentTab.extension.mediainfo = {
 	attach : function(data,div)
 	{
 		var files = $("<ul />");
-		
+		var hidden = [];
 		for(filename in data.files)
 		{
 			var trackList = $("<ul />");
@@ -14,14 +14,50 @@ Fairywren.torrentTab.extension.mediainfo = {
 			for(var i = 0; i != tracks.length; ++i)
 			{
 				var track = tracks[i];
+				
+				var showTrack = track.type in Fairywren.torrentTab.extension.mediainfo.allowedTracks;
+				
 				var trackKvs = $("<ul />");
 				for( k in track )
 				{
 					var v = $("<span />").text(track[k]);
-					var k = $("<span />").text(k);
-					trackKvs.append($("<li />").append(k,$("<span />").text(': '),v));
+					
+					var kText = k;
+					
+					var showKey = false;
+					if(showTrack)
+					{
+						showKey = k in Fairywren.torrentTab.extension.mediainfo.allowedTracks[track.type];
+					}
+					
+					if(showKey)
+					{
+						kText = Fairywren.torrentTab.extension.mediainfo.allowedTracks[track.type][k]||kText;
+					}
+					
+					var k = $("<span />").text(kText);
+					
+					var kvElement = $("<li />").append(k,$("<span />").text(': '),v);
+					
+					if(!showKey)
+					{
+						kvElement.hide();
+						hidden.push(kvElement);
+					}
+					
+					trackKvs.append(kvElement);
 				}
-				trackList.append($("<li />").append($("<span />").text(track.type),trackKvs));
+				
+				var trackElements = $("<li />").append($("<span />").text(track.type),trackKvs);
+
+				if(!showTrack)
+				{
+					trackElements.hide();
+					hidden.push(trackElements);
+				}
+
+				trackList.append(trackElements);
+				
 			}
 			
 			var fileItem = $("<li />");
@@ -29,6 +65,53 @@ Fairywren.torrentTab.extension.mediainfo = {
 			fileItem.append(trackList);
 			files.append(fileItem);
 		}
+		
+		var showAllButton = $("<span />");
+		showAllButton.text('Show All');
+		showAllButton.addClass('torrentLink');
+		showAllButton.click(function()
+		{
+			for(var i = 0 ; i != hidden.length; ++i)
+			{
+				hidden[i].show();
+			}
+			$(this).remove();
+		})
+		
+		div.append($("<div />").append(showAllButton));
 		div.append(files);
 	}
+};
+
+Fairywren.torrentTab.extension.mediainfo.allowedTracks = {
+	'General' : {
+		'Format' : null,
+		'Duration' : null,
+		'File_size' : 'File Size',
+		'Stream_size' : 'Stream Size'
+	},
+	
+	'Video' : {
+		'Width':null,
+		'Height':null,
+		'Frame_rate' : 'Frame Rate',
+		'Bit_rate' : 'Bit Rate',
+		'Stream_size' : 'Stream Size'
+		},
+	
+	'Audio' : {
+		'Compression_mode' : 'Compression Mode',
+		'Language' : null,
+		'Bit_rate' : 'Bit Rate',
+		'Channel_s_' : 'Channels',
+		'Format' : null,
+		'Stream_size' : 'Stream Size',
+		'Duration' : null,
+		'Sampling_rate' : 'Sampling rate'
+	},
+	
+	'Text': {
+		'Language' : null
+		}
+	
 };
