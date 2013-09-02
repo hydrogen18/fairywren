d05b0b310bdd1287166d46fc920b3405cf85c9ee
Eric Urban <hydrogen18@gmail.com>
2013-09-02 15:54:44 -0400

    torrent page ported, need to test the mediainfo stuff

---
+++ html/js/torrent.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+	$("body").prepend(Fairywren.makeNavbar());
+	var hash = window.location.hash;
+	
+	Fairywren.torrent.alert = $("#torrent").find("#alert");
+	Fairywren.torrent.title = $("#torrent").find("a#title");
+	Fairywren.torrent.info = $("#torrent").find("#info");
+	
+	if(hash.length === 0)
+	{
+		//User got here on accident or something. Display error message
+		//and depart
+		Fairywren.torrent.alert.append(Fairywren.makeErrorElement("You seem to have reached this page in error",true));
+		return;
+	}
+	
+	Fairywren.torrent.href = hash.slice(1);
+	
+	jQuery.get(Fairywren.torrent.href).
+	done(
+		function(data)
+		{
+			if(! Fairywren.isError(data))
+			{
+				Fairywren.torrent.data = data;
+				Fairywren.torrent();
+			}
+		}
+		).fail(Fairywren.handleServerFailure(Fairywren.torrent.alert) );
+	
+});
+
+Fairywren.torrent = function()
+{
+	Fairywren.torrent.title.attr('href',Fairywren.torrent.data.metainfo.href);
+	Fairywren.torrent.title.text(Fairywren.torrent.data.title);
+	
+	Fairywren.torrent.info.append($("<dt />").text("Uploaded"));
+	Fairywren.torrent.info.append($("<dd />").text(Fairywren.trimIsoFormatDate(Fairywren.torrent.data.creationDate)));
+	
+	Fairywren.torrent.info.append($("<dt />").text("Uploader"));
+	Fairywren.torrent.info.append($("<dd />").text(Fairywren.torrent.data.creator.name));
+	
+	Fairywren.torrent.info.append($("<dt />").text("Size"));
+	Fairywren.torrent.info.append($("<dd />").text(Fairywren.bytesToPrettyPrint(Fairywren.torrent.data.lengthInBytes)));
+	
+	if(('extended' in Fairywren.torrent.data) && Fairywren.torrent.data.extended !== null)
+	{
+		for(extension in Fairywren.torrent.data.extended)
+		{
+			if( ! (extension in Fairywren.torrent.extensions))
+			{
+				return;
+			}
+			
+			var row = $("<div />");
+			row.addClass('row');
+			
+			var data = Fairywren.torrent.data.extended[extension];
+			
+			row.append(Fairywren.torrent.extensions[extension](data));
+			
+			$("body").append(row);
+			
+		}		
+	}
+};	
+
+Fairywren.torrent.extensions = {};
+
