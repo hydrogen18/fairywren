011598757885de5b13a4385aff98794fc15d0de6
Eric Urban <hydrogen18@gmail.com>
2013-09-02 13:51:36 -0400

    not exactly what I would call code re-use, but w/e

--- html/js/search.js
+++ html/js/search.js
@@ -1,18 +1,50 @@
 
-
-Fairywren.search = {};
-Fairywren.search.init = function()
-{
-	Fairywren.search.torrentTable = $("#torrentSearch").find("#torrentTable");
-	Fairywren.search.torrentTable.hide();
-	Fairywren.search.searchbox = $("#torrentSearch").find("#searchTokens");
-	Fairywren.search.msg = $("#torrentSearch").find("#msg");
+$(document).ready(function(){
+	jQuery.ajaxSettings.traditional = true;
+	Fairywren.search.alert = $("#searchAlert");
+	Fairywren.search.searchBox = $("#searchTokens");
 	
+	Fairywren.search.table = $("#searchTable");
+	
+	$("body").prepend(Fairywren.makeNavbar());
+});
+
+Fairywren.search = function(){
+	Fairywren.search.alert.find('div').remove();
+	var tokens = Fairywren.search.extractTokens();
+	
+	if (tokens.length === 0)
+	{
+		return;
+	}
+	
+	Fairywren.search.table.find('tbody').remove();
+	
+	jQuery.get('api/torrents',{ search:1 , "token" : tokens }).done(
+		function(data)
+		{
+			
+			if(! Fairywren.isError(data))
+			{
+				if ( data.torrents.length === 0)
+				{
+					Fairywren.search.alert.append(Fairywren.makeErrorElement('Sorry, no results were found'));
+					return;
+				}
+				
+				var tmp = { pages : [data.torrents], currentPage : 0, table:Fairywren.search.table, pageIndicator: $('<p />')};
+				
+				TorrentPaginator.prototype.show.apply(tmp,[]);
+				
+			}
+		
+		}
+		).fail( Fairywren.handleServerFailure(Fairywren.search.alert));
 }
 
 Fairywren.search.extractTokens = function()
 {
-	var tokens = Fairywren.search.searchbox.val().split(' ');
+	var tokens = Fairywren.search.searchBox.val().split(' ');
 	
 	for(var i = tokens.length-1; i >= 0 ; --i)
 	{
@@ -26,37 +58,3 @@ Fairywren.search.extractTokens = function()
 	return tokens;
 }
 
-Fairywren.search.search = function()
-{
-	Fairywren.search.msg.text("");
-	var tokens = Fairywren.search.extractTokens();
-	
-	if (tokens.length === 0)
-	{
-		return;
-	}
-	jQuery.get('api/torrents',{ search:1 , "token" : tokens }).done(
-		function(data)
-		{
-			if('error' in data)
-			{
-				Fairywren.errorHandler(data);
-			}
-			else
-			{
-				if(data.torrents.length == 0)
-				{
-					Fairywren.search.msg.text("Sorry, no results were found");
-					Fairywren.search.torrentTable.hide();
-					return;
-				}
-				Fairywren.search.torrentTable.show();
-				Fairywren.clearAndRenderTorrents(Fairywren.search.torrentTable,data.torrents);
-			}
-		}
-		).fail( function (jqXhr,textStatus,errorThrown )
-		{
-			Fairywren.serverErrorHandler(jqXhr,textStatus,errorThrown,Fairywren.search.msg);
-		});
-	
-}
